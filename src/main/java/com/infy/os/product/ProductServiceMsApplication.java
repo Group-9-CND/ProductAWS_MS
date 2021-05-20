fbab211a07722a1aa6f84318d2c9e032e1e3f537
Vidya <vidya.balgi@infosys.com>
2021-05-20 10:46:18 +0530

    committed

---
+++ src/main/java/com/infy/os/product/ProductServiceMsApplication.java
@@ -0,0 +1,15 @@
+package com.infy.os.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class ProductServiceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceMsApplication.class, args);
+	}
+
+}
